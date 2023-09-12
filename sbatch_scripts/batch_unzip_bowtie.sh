40fbf51b7848ad92cb264c6d81c37ea811c3046a
yunfei-dai <dai.yun@husky.neu.edu>
2023-09-11 23:22:40 -0400

    creat sbatch job for almc

--- sbatch_scripts/batch_unzip_bowtie.sh
+++ sbatch_scripts/batch_unzip_bowtie.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ## Yunfei Dai
-## 26NOV2022
+## 11SEP2023
 
-#SBATCH --array=0-11%12
-#SBATCH --ntasks=12
+#SBATCH --array=0-9%10
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 #SBATCH --time=8:00:00
 #SBATCH --job-name=unzip-bowtie
 #SBATCH --mem=100G
 #SBATCH --partition=short
-#SBATCH -o /work/geisingerlab/Yunfei/2022_Nov_ChipSeq_Analysis/Slurm_log/slurm_unzip-bowtie.%N.%j.out
-#SBATCH -e /work/geisingerlab/Yunfei/2022_Nov_ChipSeq_Analysis/Slurm_log/slurm_unzip-bowtie.%N.%j.err
+#SBATCH -o /work/geisingerlab/2023_Sep_ALMC_Jinna_ChIPSeq_analysis_YD/slurm_log/slurm_unzip-bowtie.%N.%j.out
+#SBATCH -e /work/geisingerlab/2023_Sep_ALMC_Jinna_ChIPSeq_analysis_YD/slurm_log/slurm_unzip-bowtie.%N.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=dai.yun@northeastern.edu
 
@@ -23,14 +23,15 @@ module load samtools/1.10
 source activate /work/geisingerlab/Yunfei/2022_Nov_ChipSeq_Analysis/YD_chipseq_venv
 
 #Directories
-WORK_DIR=/work/geisingerlab/Yunfei/2022_Nov_ChipSeq_Analysis
-FASTQ_DIR=$WORK_DIR/191002-0357H_Eddie_Geisinger_5583/fastq_Lane5
+DATA_DIR=/work/geisingerlab/2023_Sep_ALMC_Jinna_ChIPSeq_analysis
+FASTQ_DIR=$DATA_DIR/201028-0304M_Edward_Geisinger_6095/fastq_Lane1
+WORK_DIR=/work/geisingerlab/2023_Sep_ALMC_Jinna_ChIPSeq_analysis_YD
 UNZIPPED=$WORK_DIR/Unzipped_fastq
 CLIPPED=$WORK_DIR/Clipped_fastq
 MAPPED_SAM=$WORK_DIR/Mapped_SAM
 MAPPED_BAM=$WORK_DIR/Mapped_BAM
 BigWig=$WORK_DIR/BigWig
-REF=$WORK_DIR/References/Ab_all/Ab_all
+REF=/work/geisingerlab/Yunfei/Ab_reference/Ab_all/Ab_all
 CLIPPER=/work/geisingerlab/software/fastx_toolkit/bin/fastx_clipper
 ADAPTOR=CTGTCTCTTATACACATCTCCGAGCCCACGAGAC
 
@@ -42,20 +43,20 @@ mkdir -p $MAPPED_BAM
 mkdir -p $BigWig
 
 #Array of inputs
-INPUT=( $(find $FASTQ_DIR -type f -name "BfmR-ChIP*.gz") )
+INPUT=( $(find $FASTQ_DIR -type f -name "*.gz") )
 func () {
     gz_file=$1
     fname=$(basename $gz_file)
     echo "processing ${gz_file}"
     unzipped_fastq=$UNZIPPED/${fname%%.gz}
     clipped_fastq=$CLIPPED/${fname%%.fastq.gz}.clipped.fastq
-    SAM_output=$MAPPED_SAM/${fname%%.fastq.gz}.sam
+    BAM_output=$MAPPED_SAM/${fname%%.fastq.gz}.bam
     BAM_sorted=$MAPPED_BAM/${fname%%.fastq.gz}.sorted.bam
     BW=$BigWig/${fname%%.fastq.gz}.bw
     gzip -cd $gz_file > $unzipped_fastq
     $CLIPPER -v -l 20 -d 0 -Q 33 -a $ADAPTOR -i $unzipped_fastq -o $clipped_fastq
-    bowtie -q -m 1 -n 1 --best -y -S $REF $clipped_fastq $SAM_output
-    samtools sort $SAM_output -o $BAM_sorted
+    bowtie -q -m 1 -n 1 --best -y -S $REF $clipped_fastq $SAM_output | samtools view -bS - > $BAM_output
+    samtools sort $BAM_output -o $BAM_sorted
     samtools index $BAM_sorted
     bamCoverage -b $BAM_sorted -o $BW --normalizeUsing CPM --binSize 20 # 16Jan2023 updated binsize and normalizing method
 }
